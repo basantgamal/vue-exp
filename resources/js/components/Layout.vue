abc33efe8e0b732b2725d6df1620ed0c5783b846
Bassant Gamal <basantgamal71@gmail.com>
2019-01-03 18:55:46 +0200

    dropdown single and multi

--- resources/js/components/Layout.vue
+++ resources/js/components/Layout.vue
@@ -1,13 +1,23 @@
 <template>
-    <div>Layout</div>
+    <div>
+        <multiselect v-model="value" :options="options"></multiselect>
+
+    </div>
 </template>
 <script>
+    import Multiselect from 'vue-multiselect'
     export default {
         name: "Layout",
+        components: {Multiselect},
         data(){
             return {
-
+                value: null,
+                options: ['list', 'of', 'options']
             }
         }
     }
 </script>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+<style lang="scss">
+
+</style>
